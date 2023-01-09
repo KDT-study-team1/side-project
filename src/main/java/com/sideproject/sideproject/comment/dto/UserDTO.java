fecdce67bc2631e395f530e88f03eede2ef1eb5a
jisoooit <ydsn336@naver.com>
2023-01-09 14:56:12 +0900

    feat: #9 Comment DTO 에 필요한 UserDTO 추가

---
+++ src/main/java/com/sideproject/sideproject/comment/dto/UserDTO.java
@@ -0,0 +1,32 @@
+package com.sideproject.sideproject.comment.dto;
+
+import com.sideproject.sideproject.customer.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String nickname;
+    private String dongName;
+    private String profile;
+
+    public static UserDTO of(
+            Long id,
+            String nickname,
+            String dongName,
+            String profile
+    ){
+        return new UserDTO(id, nickname, dongName, profile);
+    }
+
+    public static UserDTO from(User user){
+        return UserDTO.of(
+                user.getId(),
+                user.getNickname(),
+                user.getDongName(),
+                user.getProfile()
+        );
+    }
+}
