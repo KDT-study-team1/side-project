1f145c1e5573a0d0338d05601d7c774955846146
jisoooit <ydsn336@naver.com>
2023-01-16 16:29:36 +0900

    refactor: responseDTO 수정 (기본값 설정) #22

--- src/main/java/com/sideproject/sideproject/global/response/ResponseDTO.java
+++ src/main/java/com/sideproject/sideproject/global/response/ResponseDTO.java
@@ -3,33 +3,28 @@ package com.sideproject.sideproject.global.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Builder
 @Data
 public class ResponseDTO<T> {
 
+    @Schema(name = "success", example = "true")
+    private Boolean success;
+
     @Schema(name = "Http code", example = "200")
-    private int state;
-
-    @Schema(name = "result code", example = "success")
-    private String result;
-
+    private Integer code;
     @Schema(name = "result message", example = "응답 성공")
     private String message;
 
     @Schema(name = "result date", example = "{name = '철수김', age = '25세'}")
     private T data;
 
-    public ResponseDTO(int state, String result, String message, T data) {
-        this.state = state;
-        this.result = result;
-        this.message = message;
+    public ResponseDTO(T data) {
+        this.success = true;
+        this.code = HttpStatus.OK.value();
+        this.message = "요청에 성공하였습니다.";
         this.data = data;
     }
 
-    public ResponseDTO(int state, String result, String message) {
-        this.state = state;
-        this.result = result;
-        this.message = message;
-    }
 }
