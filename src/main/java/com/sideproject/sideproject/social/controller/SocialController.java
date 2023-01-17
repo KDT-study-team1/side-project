0261f92b11f133d12453c8dfa2cc2fa2a2eb18b1
JeYeongR <wpdud2003@gmail.com>
2023-01-17 20:57:49 +0900

    style: SocialController에 swagger 설명 추가

--- src/main/java/com/sideproject/sideproject/social/controller/SocialController.java
+++ src/main/java/com/sideproject/sideproject/social/controller/SocialController.java
@@ -3,6 +3,7 @@ package com.sideproject.sideproject.social.controller;
 import com.sideproject.sideproject.social.dto.SocialRequestDTO;
 import com.sideproject.sideproject.social.dto.SocialResponseDTO;
 import com.sideproject.sideproject.social.service.SocialService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +18,13 @@ public class SocialController {
     private final SocialService socialService;
 
     @PostMapping("/social")
+    @Operation(summary = "모임 게시글 생성", description = "모임 게시글 생성 api")
     public String createSocial(SocialRequestDTO socialRequestDTO) {
         return socialService.createSocial(socialRequestDTO);
     }
 
     @GetMapping("/socials")
+    @Operation(summary = "모임 게시글 전체 조회", description = "모임 게시글 전체 조회 api")
     public List<SocialResponseDTO> socials() {
         return socialService.socials();
     }
