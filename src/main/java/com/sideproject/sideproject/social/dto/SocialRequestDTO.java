6d14b4d1d4627c3c426c81783dca172e4f770d56
JeYeongR <wpdud2003@gmail.com>
2023-01-17 14:30:46 +0900

    style: SocialRequestDTO에 swagger 설명 추가

--- src/main/java/com/sideproject/sideproject/social/dto/SocialRequestDTO.java
+++ src/main/java/com/sideproject/sideproject/social/dto/SocialRequestDTO.java
@@ -5,39 +5,56 @@ import com.sideproject.sideproject.post.domain.PostImage;
 import com.sideproject.sideproject.social.domain.Social;
 import com.sideproject.sideproject.tag.domain.Category;
 import com.sideproject.sideproject.tag.domain.SocialTag;
-import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Schema(description = "모임 게시글 생성시 입력받는 DTO")
+@Getter
+@Setter
 public class SocialRequestDTO {
 
+    @Schema(description = "user_id", defaultValue = "2")
     private User user;
 
+    @Schema(description = "모임 게시글 이미지", defaultValue = "사진")
     private List<PostImage> images = new ArrayList<>();
 
+    @Schema(description = "모임 게시글 내용", defaultValue = "집가고 싶다")
     private String contents;
 
+    @Schema(description = "지역 코드", defaultValue = "12345")
     private Integer regionCode;
 
+    @Schema(description = "행정 구역 코드", defaultValue = "055")
     private Long dongCode;
 
+    @Schema(description = "동 이름", defaultValue = "우리집")
     private String dongName;
 
+    @Schema(description = "카테고리", defaultValue = "null")
     private Category category;
 
+    @Schema(description = "소셜 태그", defaultValue = "null")
     private List<SocialTag> socialTags = new ArrayList<>();
 
+    @Schema(description = "모임 게시글 제목", defaultValue = "같이 놀사람")
     private String title;
 
+    @Schema(description = "시작 날짜")
     private LocalDateTime startDate;
 
+    @Schema(description = "종료 날짜")
     private LocalDateTime endDate;
 
+    @Schema(description = "모임 최대 참가수", defaultValue = "5")
     private Integer limitedNums;
 
+    @Schema(description = "연락수단", defaultValue = "010-1234-5678")
     private String contact;
 
     public Social toEntity() {
