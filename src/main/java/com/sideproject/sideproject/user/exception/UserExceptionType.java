9d28d25aed4581902f1dcd68f986851ac1dcfe48
HongGuma <hongsu9849@gmail.com>
2023-01-18 16:16:21 +0900

    Add : 로그인시 발생하는 예외 추가

--- src/main/java/com/sideproject/sideproject/user/exception/UserExceptionType.java
+++ src/main/java/com/sideproject/sideproject/user/exception/UserExceptionType.java
@@ -3,7 +3,9 @@ package com.sideproject.sideproject.user.exception;
 import com.sideproject.sideproject.global.exception.CustomExceptionType;
 
 public enum UserExceptionType implements CustomExceptionType {
-    DUPLICATE_EMAIL(-101,"이미 존재하는 email입니다.");
+    DUPLICATE_EMAIL(-101,"이미 존재하는 `email`입니다."),
+    NON_EXISTENT_USER(-102, "존재하지 않는 회원입니다."),
+    UNMATCHED_PASSWORD(-103, "일치하지 않는 패스워드");
 
     private int errorCode;
 
