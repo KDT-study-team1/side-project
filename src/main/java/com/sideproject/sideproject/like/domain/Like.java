68dbe8a48a2caa97df88f54d4fc9ff3224e34d14
HongGuma <hongsu9849@gmail.com>
2023-01-18 19:50:17 +0900

    add : like entity 추가

---
+++ src/main/java/com/sideproject/sideproject/like/domain/Like.java
@@ -0,0 +1,41 @@
+package com.sideproject.sideproject.like.domain;
+
+import com.sideproject.sideproject.post.domain.Post;
+import com.sideproject.sideproject.user.domain.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "LIKES")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; //좋아요 누른 회원
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post; //좋아요 단 글
+
+
+    @Builder
+    private Like(User user, Post post){
+        this.user = user;
+        this.post = post;
+    }
+
+    public static Like createLike(User user, Post post){
+        Like like = Like.builder().user(user).post(post).build();
+        return like;
+    }
+
+}
