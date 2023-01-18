c22e99f25cac329ab0f22f763110039fb9a4480e
Darby <lkh55913@naver.com>
2023-01-18 16:52:18 +0900

    feat: #18 Tag 번호 기준 Social 검색 Repository 생성
    Tag 번호를 바탕으로 Social 검색하는 Repository 생성

---
+++ src/main/java/com/sideproject/sideproject/search/repository/SearchSocialRepository.java
@@ -0,0 +1,16 @@
+package com.sideproject.sideproject.search.repository;
+
+import com.sideproject.sideproject.social.domain.Social;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SearchSocialRepository extends JpaRepository<Social, Long> {
+
+    @Query("SELECT s FROM Social s WHERE s.id IN " +
+            "(SELECT st.social.id from SocialTag st where st.tag.id = :tagNum )")
+    List<Social> findByTagId(@Param("tagNum") Long tagNum);
+
+}
