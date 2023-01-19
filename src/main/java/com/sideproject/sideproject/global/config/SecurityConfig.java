402f3b65677d04dc086e4218eed5d42cf0fe06eb
HongGuma <hongsu9849@gmail.com>
2023-01-19 21:52:45 +0900

    style : 주석 추가

--- src/main/java/com/sideproject/sideproject/global/config/SecurityConfig.java
+++ src/main/java/com/sideproject/sideproject/global/config/SecurityConfig.java
@@ -21,6 +21,9 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        String[] matchers = {"/", "/login/**", "/users", "/refresh", "/swagger-ui/**", "/api-docs/**"};
+
+        //login
         http
                 .cors()//기본 cors 설정
                 .and()
@@ -29,11 +32,10 @@ public class SecurityConfig{
                 .httpBasic().disable() //httpBasic 인증 비활성화
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers(
-                        "/oauth2/**", "/", "/login/**", "/users", "/refresh", "/swagger-ui/**", "/api-docs/**")
-                .permitAll()
-                .anyRequest().authenticated();
+                .authorizeRequests().antMatchers(matchers).permitAll() //모두에게 허용할 URL
+                .anyRequest().authenticated(); //인증이 되었는지 검증
 
+        //logout
         http
                 .logout().permitAll()
                 .logoutUrl("/logout");
