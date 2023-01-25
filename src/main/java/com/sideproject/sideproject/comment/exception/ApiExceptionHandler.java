8692b05a4dd328240a2ab104c3498f483450411b
jisoooit <ydsn336@naver.com>
2023-01-25 17:02:33 +0900

    feat: ApiExceptionHandler log 추가

--- src/main/java/com/sideproject/sideproject/comment/exception/ApiExceptionHandler.java
+++ src/main/java/com/sideproject/sideproject/comment/exception/ApiExceptionHandler.java
@@ -2,18 +2,20 @@ package com.sideproject.sideproject.comment.exception;
 
 import com.sideproject.sideproject.global.exception.CustomException;
 import com.sideproject.sideproject.global.response.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
+@Slf4j
 public class ApiExceptionHandler {
 
     @ExceptionHandler
-    public ErrorResponseDTO validation(ConstraintViolationException e, WebRequest request) {
+    public ErrorResponseDTO validation(ConstraintViolationException e) {
+        log.error("error name : {}",e.toString());
         return ErrorResponseDTO.builder()
                 .errorCode(GlobalExceptionType.VALIDATION_ERROR.getErrorCode())
                 .message(GlobalExceptionType.VALIDATION_ERROR.getErrorMsg())
@@ -21,8 +23,8 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler
-    public ErrorResponseDTO validation(MethodArgumentNotValidException e, WebRequest request) {
-        System.out.println("methodArgumentNotValidException");
+    public ErrorResponseDTO validation(MethodArgumentNotValidException e) {
+        log.error("error name : {}",e.toString());
         return ErrorResponseDTO.builder()
                 .errorCode(GlobalExceptionType.VALIDATION_ERROR.getErrorCode())
                 .message(GlobalExceptionType.VALIDATION_ERROR.getErrorMsg())
@@ -30,8 +32,8 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler
-    public ErrorResponseDTO global(GlobalException e, WebRequest request) {
-        System.out.println("global");
+    public ErrorResponseDTO global(GlobalException e) {
+        log.error("error name : {}",e.getGlobalExceptionType().getErrorMsg());
         return ErrorResponseDTO.builder()
                 .errorCode(e.getGlobalExceptionType().getErrorCode())
                 .message(e.getGlobalExceptionType().getErrorMsg())
@@ -39,8 +41,8 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler
-    public ErrorResponseDTO global(CustomException e, WebRequest request) {
-        System.out.println("global");
+    public ErrorResponseDTO global(CustomException e) {
+        log.error("error name : {}",e.getCustomExceptionType().getErrorMsg());
         return ErrorResponseDTO.builder()
                 .errorCode(e.getCustomExceptionType().getErrorCode())
                 .message(e.getCustomExceptionType().getErrorMsg())
@@ -48,8 +50,8 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler
-    public ErrorResponseDTO exception(Exception e, WebRequest request) {
-        System.out.println("exception");
+    public ErrorResponseDTO exception(Exception e) {
+        log.error("error name : {}",e.toString());
         return ErrorResponseDTO.builder()
                 .errorCode(GlobalExceptionType.INTERNAL_ERROR.getErrorCode())
                 .message(GlobalExceptionType.INTERNAL_ERROR.getErrorMsg())
