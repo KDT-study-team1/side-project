3f167e2544c4ac09a33a52c9ebcbf92d5f5ff7a6
zoomin3022 <zoomin3022@gmail.com>
2023-01-31 21:26:29 +0900

    feat: #13 LikeController에 displayLikes 메소드 추가

--- src/main/java/com/sideproject/sideproject/like/controller/LikeController.java
+++ src/main/java/com/sideproject/sideproject/like/controller/LikeController.java
@@ -1,5 +1,6 @@
 package com.sideproject.sideproject.like.controller;
 
+import com.sideproject.sideproject.global.response.ResponseDTO;
 import com.sideproject.sideproject.like.dto.LikeResponseDto;
 import com.sideproject.sideproject.like.service.LikeService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,17 +16,24 @@ public class LikeController {
 
     private final LikeService likeService;
 
+    @GetMapping("/{postId}")
+    public ResponseDTO displayLikes(@PathVariable Long postId) {
+        return new ResponseDTO(likeService.displayLikes(postId));
+    }
+
     @PostMapping("/{postId}")
-    @Operation(summary = "좋아요 버튼",description = "좋아요가 없는 상태에서 실행하면 생기고 반대면 사라진다")
+    @Operation(summary = "좋아요 버튼", description = "좋아요가 없는 상태에서 실행하면 생기고 반대면 사라진다")
     //일단은 RequestParam으로 userId 받았다가 나중에 로그인 합쳐지면 변경예정
     //아마 @AuthenticationPrincipal 써야되는거 같음?
-    public LikeResponseDto setLike(@PathVariable Long postId, @RequestParam Long userId) {
-        boolean isLiked = likeService.likeButton(userId, postId);
+    public ResponseDTO setLike(@PathVariable Long postId, @RequestParam Long userId) {
 
-        return LikeResponseDto.builder()
+        boolean isLiked = likeService.likeButton(userId, postId);
+        LikeResponseDto likeResponseDto = LikeResponseDto.builder()
                 .userId(userId)
                 .postId(postId)
                 .isLiked(isLiked)
                 .build();
+
+        return new ResponseDTO(likeResponseDto);
     }
 }
