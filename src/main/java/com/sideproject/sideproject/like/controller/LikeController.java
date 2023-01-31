411fccd38f0f8c3b78ee7658f844aff3a40dde2f
zoomin3022 <zoomin3022@gmail.com>
2023-01-31 16:00:31 +0900

    feat: #13 좋아요 버튼 기능 Swagger 어노테이션 추가

--- src/main/java/com/sideproject/sideproject/like/controller/LikeController.java
+++ src/main/java/com/sideproject/sideproject/like/controller/LikeController.java
@@ -2,19 +2,20 @@ package com.sideproject.sideproject.like.controller;
 
 import com.sideproject.sideproject.like.dto.LikeDto;
 import com.sideproject.sideproject.like.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "like", description = "좋아요 API")
 public class LikeController {
 
     private final LikeService likeService;
 
     @PostMapping("/like/{postId}")
+    @Operation(summary = "좋아요 버튼",description = "좋아요가 없는 상태에서 실행하면 생기고 반대면 사라진다")
     //일단은 RequestParam으로 userId 받았다가 나중에 로그인 합쳐지면 변경예정
     //아마 @AuthenticationPrincipal 써야되는거 같음?
     public LikeDto setLike(@PathVariable Long postId, @RequestParam Long userId) {
--- src/main/java/com/sideproject/sideproject/like/dto/LikeDto.java
+++ src/main/java/com/sideproject/sideproject/like/dto/LikeDto.java
@@ -1,12 +1,17 @@
 package com.sideproject.sideproject.like.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Schema(description = "좋아요 버튼 실행 시 response DTO")
 public class LikeDto {
+    @Schema(description = "좋아요가 남겨진 Post Id")
     private Long postId;
+    @Schema(description = "좋아요를 누른 User Id")
     private Long userId;
+    @Schema(description = "true면 좋아요가 눌러진 상태 false면 해제된 상태")
     private boolean isLiked;
 }
