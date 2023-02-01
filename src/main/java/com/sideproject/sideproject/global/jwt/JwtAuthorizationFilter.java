2d55087e70782c12ce95c00d2b312a6b07b1dd9d
HongGuma <hongsu9849@gmail.com>
2023-02-01 21:52:43 +0900

    conf : Jwt 인증 필터 생성

---
+++ src/main/java/com/sideproject/sideproject/global/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,88 @@
+package com.sideproject.sideproject.global.jwt;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sideproject.sideproject.global.CustomUserDetails;
+import com.sideproject.sideproject.user.domain.User;
+import com.sideproject.sideproject.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 토큰 인증 담당
+ *
+ * 1. cookie에서 jwt token을 가져온다.
+ * 2. token을 파싱해서 user email을 구한다.
+ * 3. user email로 User 엔티티를 가져오고 Authentication을 생성한다.
+ * 4. 생성된 Authentication을 SecurityContext에 넣는다.
+ * 5. Exeption이 발생하면 response의 cookie를 null 변경한다.
+ */
+@Slf4j
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private UserRepository userRepository;
+
+    public JwtAuthorizationFilter(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        log.info("JWT filter is running...");
+
+        String token = null;
+        try { //쿠키에서 JWT 토큰을 가져오기
+            token = Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME)) //필요한 쿠키만 가져오기
+                    .findFirst()
+                    .map(Cookie::getValue) //쿠키 값 가져오기
+                    .orElse(null); //못 찾아오면 null
+        }catch (Exception ignored){
+            //아무것도 안함?
+        }
+
+        if(token != null){
+            try{
+                //athentication을 만들어서 SecurityContext에 넣는다.
+                Authentication authentication = this.getUsernamePasswordAuthenticationToken(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (Exception e){
+                //실패하면 cookie 초기화 (실패하면 쿠키가 쓸모 없어져서)
+                Cookie cookie = new Cookie(JwtProperties.COOKIE_NAME,null);
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * JWT 토큰으로 User를 찾아서 UsernamePasswordAuthenticationToken으로 만들어서 리턴
+     * User가 없으면 null
+     */
+    private Authentication getUsernamePasswordAuthenticationToken(String token){
+        String userEmail = JwtUtils.getUserEmail(token);
+        if(userEmail != null){
+            return new UsernamePasswordAuthenticationToken(userEmail, null, AuthorityUtils.NO_AUTHORITIES);
+        }
+        return null;
+    }
+}
--- src/main/java/com/sideproject/sideproject/global/jwt/JwtUtils.java
+++ src/main/java/com/sideproject/sideproject/global/jwt/JwtUtils.java
@@ -1,9 +1,10 @@
 package com.sideproject.sideproject.global.jwt;
 
 import com.sideproject.sideproject.user.domain.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwsHeader;
-import io.jsonwebtoken.Jwts;
+import com.sideproject.sideproject.user.repository.UserRepository;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.util.Pair;
 
 import java.security.Key;
@@ -12,46 +13,45 @@ import java.util.Date;
 /**
  * 토큰 생성하고 검증하는 객체
  */
+@Slf4j
 public class JwtUtils {
 
-    /**
-     * 토큰에서 userEmail 추출
-     * Token -> clamis
-     *
-     * @param token : 토큰
-     * @return user email
-     */
-    public static String getUserEmail(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKeyResolver(SigningKeyResolver.instance)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
+    private static Pair<String, Key> keyPair = JwtKey.getRandomKey(); // key,value 쌍으로 저장됨
 
     /**
      * User의 email로 토큰 생성
-     * Header : alg, kid
-     * Payload : sub, iat, exp
-     * Signature : JwtKey.getRandomKey로 구한 Secret Key를 HS512 암호화
-     *
-     * @param user : 사용자 객체
-     * @return Jwt Token
      */
     public static String createToken(User user) {
-        Claims claims = Jwts.claims().setSubject(user.getEmail()); //sub
+        Claims claims = Jwts.claims().setSubject(user.getEmail()); //sub : payload에 저장되는 정보 단위
         Date now = new Date();
-        Pair<String, Key> key = JwtKey.getRandomKey();
 
-        //Jwt 토큰 생성
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
+        String token = Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(new Date()) //토큰 발행 시간
                 .setExpiration(new Date(now.getTime() + JwtProperties.EXPIRATION_TIME)) //토큰 만료시간
-                .setHeaderParam(JwsHeader.KEY_ID, key.getFirst())
-                .signWith(key.getSecond())
+                .setHeaderParam(JwsHeader.KEY_ID, keyPair.getFirst()) //헤더에 kid 넣기
+                .signWith(keyPair.getSecond(), SignatureAlgorithm.HS512) //알고리즘
                 .compact();
+
+        log.info("createAuthToken: {}", token);
+
+        return token;
+    }
+
+    /**
+     * 토큰에서 userEmail 추출
+     */
+    public static String getUserEmail(String token) {
+        String email = Jwts.parserBuilder()
+                .setSigningKeyResolver(SigningKeyResolver.instance) //kid를 찾아서 시그니처 검증
+                .build()
+                .parseClaimsJws(token) //claims 파싱, 실패시 SignatureException 발생
+                .getBody() // subject 찾기
+                .getSubject();
+
+        log.info("get email for token : {}", email);
+
+        return email;
     }
 
 }
