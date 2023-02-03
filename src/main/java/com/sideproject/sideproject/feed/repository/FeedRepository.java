c0b7a9040b8e081b3d79494e6083a7264a2935af
jisoooit <ydsn336@naver.com>
2023-02-03 15:46:03 +0900

    feat: FeedRepository 메서드 추가

--- src/main/java/com/sideproject/sideproject/feed/repository/FeedRepository.java
+++ src/main/java/com/sideproject/sideproject/feed/repository/FeedRepository.java
@@ -4,6 +4,11 @@ package com.sideproject.sideproject.feed.repository;
 import com.sideproject.sideproject.feed.domain.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedRepository extends JpaRepository<Feed, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+    Optional<Feed> findByIdAndUserEmail(Long postId, String email);
+
+    List<Feed> findAllByUserEmail(String email);
 }
