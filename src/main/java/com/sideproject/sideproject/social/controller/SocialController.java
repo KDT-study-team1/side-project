1cd7f6b91185d7371ce93bd6b79f10fd13c1ea89
JeYeongR <wpdud2003@gmail.com>
2023-02-06 15:42:05 +0900

    feat: #44 SocialController에 모임 게시글 상세 정보 조회 api 추가

--- src/main/java/com/sideproject/sideproject/social/controller/SocialController.java
+++ src/main/java/com/sideproject/sideproject/social/controller/SocialController.java
@@ -1,15 +1,13 @@
 package com.sideproject.sideproject.social.controller;
 
 import com.sideproject.sideproject.global.response.ResponseDTO;
+import com.sideproject.sideproject.social.dto.SocialDetailResponseDTO;
 import com.sideproject.sideproject.social.dto.SocialRequestDTO;
 import com.sideproject.sideproject.social.dto.SocialResponseDTO;
 import com.sideproject.sideproject.social.service.SocialService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,13 @@ public class SocialController {
         List<SocialResponseDTO> socials = socialService.socials();
 
         return new ResponseDTO<>(socials);
+    }
+
+    @GetMapping("/social/{socialId}")
+    @Operation(summary = "모임 게시글 상세 정보 조회", description = "모임 게시글 상세 정보 조회 api")
+    public ResponseDTO<?> socialDetail(@PathVariable Long socialId) {
+        SocialDetailResponseDTO social = socialService.socialDetail(socialId);
+
+        return new ResponseDTO<>(social);
     }
 }
